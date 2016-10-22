fd6a61e520b7c389f9bb4e582945d7a62e94c64d
Yuli <Yuli@Yulis-MacBook-Pro.local>
2016-10-22 01:22:54 -0400

    initial update

---
+++ KitCat/app/src/main/java/com/example/yuli/kitcat/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yuli.kitcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+/Users/Yuli/AndroidStudioProjects/KitCat/app/src/main/java/com/example/yuli/kitcat/MainActivity.java
